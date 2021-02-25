6c6ffcf2c7a2f2392d4a59d683b2264a8891e0ce
YoViktar <victordevguy@gmail.com>
2021-02-25 20:03:55 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 exports.min = function min(array) {
     if (!array || !array.length) return 0;
-    let min = array.reduce(function (a, b) {
-        return Math.min(a, b);
-    });
-    return min;
+    return Math.min(...array);
 };
 
 exports.max = function max(array) {
     if (!array || !array.length) return 0;
-    let max = array.reduce(function (a, b) {
-        return Math.max(a, b);
-    });
-    return max;
+    return Math.max(...array);
 };
 
 exports.avg = function avg(array) {
